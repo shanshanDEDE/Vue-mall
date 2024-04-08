60dd0cf93e1038eadbe0fa19fc9016f34f6cf510
willy <willyjen1111@gmail.com>
2024-04-08 13:43:33 +0800

    20240408

--- src/views/MemberTrack.vue
+++ src/views/MemberTrack.vue
@@ -42,60 +42,6 @@
           </div>
         </div>
       </div>
-
-      <div class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/1.webp" class="d-block w-100" />
-          </div>
-          <div class="carousel-item">
-            <img src="/2.webp" class="d-block w-100" />
-          </div>
-          <div class="carousel-item">
-            <img src="/3.webp" class="d-block w-100" />
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
     </main>
   </main>
 </template>
@@ -151,12 +97,6 @@ export default {
           // 处理错误
           console.error(error);
         });
-    },
-
-    // 登錄成功後調用此方法，更新 trackDTO 中的用戶信息
-    handleLoginSuccess(userId) {
-      this.trackDTO.userId = userId;
-      // 如果還有其他需要初始化的屬性，可以在這裡進行設置
     },
   },
   components: {
