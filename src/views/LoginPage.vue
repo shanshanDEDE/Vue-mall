b66335135f7ddcab9a179d9a0fc56e4d6b671f1d
wei <891001kevin@gmail.com>
2024-04-10 10:04:41 +0800

    4/10 problem fixed

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,12 +2,27 @@
   <main class="container col-xl-10 col-xxl-8 px-4 py-5">
     <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
-        <img
-          src="/Apple.webp"
-          alt="NonoShop"
-          @click="check"
-          class="img-fluid mb-3"
-        />
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="@/assets/img/iphone.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="@/assets/img/macbook2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="@/assets/img/ipad.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
         <form class="p-4 p-md-5 border rounded-3 bg-light">
